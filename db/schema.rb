d0adc9613bfc8b289a50a801b69e8fa2e3a0dc7a
Pickles <cheesemaster56@gmail.com>
2022-07-27 09:10:58 -0400

    post creation

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_100755) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
